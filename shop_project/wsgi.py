6be94f5559cca7f30df0a549426a480e88d7260e
adityakumarverma2004 <adityakumarverma2004@users.noreply.github.com>
2026-04-16 14:44:04 +0530

    Fix Vercel deployment configs

--- shop_project/wsgi.py
+++ shop_project/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop_project.settings')
 
 application = get_wsgi_application()
+
+# Vercel needs an app variable to be present
+app = application
